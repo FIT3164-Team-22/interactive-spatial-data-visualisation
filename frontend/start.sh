b95ce57f3c97ce787f8da6956f6cf460a8386582
Rahul <166674500+ragingpager@users.noreply.github.com>
2025-10-04 12:17:31 +1000

    update

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 set -eu
 
-BACKEND_URL="${BACKEND_URL:-http://backend:8000}"
+DEFAULT_BACKEND_URL="http://backend:8000"
+
+if [ "${BACKEND_URL:-}" = "" ]; then
+  if [ "${BACKEND_HOST:-}" != "" ]; then
+    SCHEME="${BACKEND_SCHEME:-https}"
+    BACKEND_URL="${SCHEME}://${BACKEND_HOST}"
+    if [ "${BACKEND_PORT:-}" != "" ] && [ "${BACKEND_PORT}" != "80" ] && [ "${BACKEND_PORT}" != "443" ]; then
+      BACKEND_URL="${BACKEND_URL}:${BACKEND_PORT}"
+    fi
+  else
+    BACKEND_URL="$DEFAULT_BACKEND_URL"
+  fi
+fi
+
 export BACKEND_URL
 
 envsubst '${BACKEND_URL}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
